c90e62d637389716ee658c9696b90670ab8f8e2c
unknown <flavinhaalmeiiida234@gmail.com>
2024-07-21 16:13:49 -0300

    AA

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.use(bodyParser.json());
 
 app.use('/news', newsRoutes);
 
+
+app.get('*', (req, res) => {
+    res.status(404).json({
+      "error": "Rota não encontrada"
+    });
+  });
+  
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
